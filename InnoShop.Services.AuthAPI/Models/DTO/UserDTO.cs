253066fadf210f2c127138fbfb6cae8deb257c09
kamilla <kamilla.melikava@gmail.com>
2024-06-28 14:50:23 +0300

    Add project files.

---
+++ InnoShop.Services.AuthAPI/Models/DTO/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace InnoShop.Services.AuthAPI.Models.DTO
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+    }
+}
