e539d22f14e4928efbaabcb4872e6cedfd970d56
kamilla <kamilla.melikava@gmail.com>
2024-06-30 00:04:38 +0300

    Added JWT tokens to authentication and authorization

--- InnoShop.Services.AuthAPI/Service/AuthService.cs
+++ InnoShop.Services.AuthAPI/Service/AuthService.cs
@@ -11,10 +11,13 @@ namespace InnoShop.Services.AuthAPI.Service
         private readonly AppDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
-        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public AuthService(AppDbContext db, IJwtTokenGenerator jwtTokenGenerator,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _jwtTokenGenerator = jwtTokenGenerator;
             _userManager = userManager;
             _roleManager = roleManager;
         }
@@ -29,6 +32,9 @@ namespace InnoShop.Services.AuthAPI.Service
             }
 
             //if user was found, generate JWT token
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+
             UserDTO userDTO = new()
             {
                 Email = user.Email,
@@ -40,7 +46,7 @@ namespace InnoShop.Services.AuthAPI.Service
             LoginResponseDTO loginResponseDTO = new()
             {
                 User = userDTO,
-                Token = "",
+                Token = token,
             };
             return loginResponseDTO;
         }
